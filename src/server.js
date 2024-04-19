f8d139d12db899ff426a93d0cc701c202382eaed
临江仙 <2607881950@qq.com>
2024-04-19 20:55:27 +0800

    server

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const mainRouter = require("./routes");
-const logMiddleware = require("./utils/logMiddleware");
+// const logMiddleware = require("./utils/logMiddleware");
 // const userAuthorize = require("./utils/userAuthorize");
 const corsHandler = require("./utils/corsHandler");
 const jwtParser = require("./utils/jwtParser");
@@ -17,7 +17,7 @@ async function consturctServer(moduleDefs) {
   /* CORS 处理跨域请求 */
   app.use(corsHandler);
   /* 日志 */
-  app.use(logMiddleware());
+  // app.use(logMiddleware());
   /* 文件上载 */
   app.use(bodyParser.urlencoded({ extended: false }));
   /* 解析请求正文 支持URL编码和JSON格式 */
